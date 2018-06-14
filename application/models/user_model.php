4b23c4e282241b9e099aa87dc491cc4e8cc45857
mzwae <mrmonjaie@gmail.com>
2018-06-14 16:50:33 +0100

    added users controller

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -7,7 +7,6 @@ class User_model extends CI_Model{
     $data = array(
       'name' => $this->input->post('name'),
       'email' => $this->input->post('email'),
-      'username' => $this->input->post('username'),
       'password' => $enc_password
     );
 
