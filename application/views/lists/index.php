07ee443505866605061c6a147e15a435d8fddbac
mzwae <mrmonjaie@gmail.com>
2018-06-26 20:18:15 +0100

    added tasks progress view

--- application/views/lists/index.php
+++ application/views/lists/index.php
@@ -14,14 +14,21 @@
 <?php echo form_close(); ?>
 
 <ul class="list-group">
-  <?php foreach($lists as $list): ?>
+  <?php foreach ($lists as $list): ?>
     <li class="list-group-item">
       <a href="<?=base_url('tasks/index/'.$list['list_id'])?>">
         <?=$list['list_name']?>
       </a>
+
       <form class="list-delete" action="lists/delete/<?=$list['list_id']?>" method="post">
           <input type="submit" value="[X]" class="btn-link text-danger">
-        </form>
+      </form>
+
+      <div class="progress">
+        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="<?=$tasks_progress?>" aria-valuemin="0"   aria-valuemax="100" style="width:<?=$tasks_progress?>">
+            <?=$tasks_progress?>
+          </div>
+        </div>
 
     </li>
   <?php endforeach; ?>
